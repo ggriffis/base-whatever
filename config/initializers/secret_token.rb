ec2cc13bfd6b902d3b31c7ea11f8dae24b6748a7
Ginger Griffis <gingergriffis@hotmail.com>
2013-07-28 16:35:50 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BaseWhateverNumberTranslator::Application.config.secret_key_base = 'ceba0c165689f034856703751fbcbd6cf0c287729df6d6d06eb584edec98c59b2e6d4aeb6f2b71b84a5756738bb6ebde5181db8882a050f1dd9f3f5c6da3dae3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+BaseWhateverNumberTranslator::Application.config.session_store :cookie_store, key: '_base_whatever_number_translator_session'
