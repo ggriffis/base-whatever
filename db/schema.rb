12f95b917ab042d61823bf215d87ebd1abb10392
Ginger Griffis <gingergriffis@hotmail.com>
2013-08-04 01:49:41 -0700

    Changed to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20130801193357) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "number_calculators", force: true do |t|
     t.string  "number"
     t.integer "base"
